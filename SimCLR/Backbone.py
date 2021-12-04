9732507aa562bc7da4c55104e0295cf14b2b1966
wenbo-uo <wwu039@uottawa.ca>
2021-12-03 23:50:26 -0500

    add more files

---
+++ SimCLR/Backbone.py
@@ -0,0 +1,12 @@
+import torchvision
+
+def get_resnet(name, pretrained = False):
+ 	resnets = {"resnet18": torchvision.models.resnet18(pretrained=pretrained),
+ 			   "resnet50": torchvision.models.resnet50(pretrained=pretrained),
+ 			   "resnet101":torchvision.models.resnet101(pretrained=pretrained),
+
+ 	}
+
+ 	if name not in resnets.keys():
+ 		raise KeyError(f"{name} is not a valid ResNet version")
+ 	return resnets[name]
